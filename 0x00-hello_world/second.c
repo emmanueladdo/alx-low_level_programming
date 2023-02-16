624b9568ffb302b9111e53a411f0a089bb9fb55d
emmanueladdo <asememmanueladdo@gmial.com>
2023-02-16 10:15:21 -0800

    Compiler

---
+++ 0x00-hello_world/second.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description - Prints hello world
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+printf("Hello World!");
+return (0);
+}
