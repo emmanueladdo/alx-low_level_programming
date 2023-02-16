524edad5f45540421e7cadf9a03538f7f394d051
emmanueladdo <asememmanueladdo@gmial.com>
2023-02-16 11:09:14 -0800

    put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description - Prints hello world
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+char mywords = "Programming is like building a multilingual puzzle";
+puts(mywords);
+return (0);
+}
