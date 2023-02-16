efdcee2398a4ea12e5d1b99d1c49b75d625dbc4e
emmanueladdo <asememmanueladdo@gmial.com>
2023-02-16 11:51:50 -0800

    Sizes matters

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,5 @@ printf("Size of an int: %u byte(s)\n", sizeof(int));
 printf("Size of a long int: %u byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
 printf("Size of a float: %u byte(s)\n", sizeof(float));
-return (0)
+return (0);
 }
