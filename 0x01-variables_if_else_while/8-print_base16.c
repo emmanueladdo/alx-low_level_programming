6c58efa3f0c90a333fc021888eb42ec505dc86c0
emmanueladdo <asememmanueladdo@gmial.com>
2023-02-17 13:33:19 -0800

    Hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0(successful)
+ */
+int main(void)
+{
+char c;
+int i;
+for (i = 0; i < 10; i++)
+{
+	putchar(i + '0');
+}
+for (c = 'a'; c < 'g'; c++)
+{
+	putchar(c);
+	}
+putchar('\n');
+return (0);
+}
