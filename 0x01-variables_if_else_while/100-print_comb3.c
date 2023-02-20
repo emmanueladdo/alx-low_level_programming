0ff34add307c43de551c105d7620df5746b743a7
emmanueladdo <asememmanueladdo@gmial.com>
2023-02-20 02:22:09 -0800

    the best combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all possible combinations fo single-digit numbers.
+ * Numbers must be separated by commas and a space.
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 100; a++)
+	{
+		b = a / 10;
+		c = a % 10;
+
+		putchar(a + '0');
+		putchar(c + '0');
+
+		if (a < 99)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
