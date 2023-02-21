af9b901e7e25d8cc99e05eef9383f61025108124
emmanueladdo <asememmanueladdo@gmial.com>
2023-02-21 10:45:50 -0800

    is Alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *_islower - Checks for lower and return 1,0 if not
+ *_isalpha - Checks for lower and return 1,0 if not
  * Return: - 1 if true else 0
  * @c: char type to return
  *
