e3a6c01ffc80a49c85f186b832111ff792a43758
emmanueladdo <asememmanueladdo@gmial.com>
2023-02-21 10:02:42 -0800

    Aplah-print

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints lower alphabets using 10x
+ * Return: - Always return(0)
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int counter = 0;
+
+	while (counter <= 10)
+	{
+		a = 'a';
+		while (a <= 'z')
+		{
+		_putchar(a);
+		a++;
+		}
+		_putchar('\n');
+		counter++;
+	}
+}
