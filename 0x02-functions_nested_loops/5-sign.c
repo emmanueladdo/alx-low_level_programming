09db1e085eb1b0908b18c11d76e0a09ae4fb944b
emmanueladdo <asememmanueladdo@gmial.com>
2023-02-21 11:11:58 -0800

    sign printing

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * Return: - 1 if greater than 0 and returns -1 if less than 0
+ * @n: number to return
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
