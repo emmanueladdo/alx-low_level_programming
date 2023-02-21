02b409e8a172e833b92df5654be6a6bf3c0bf620
emmanueladdo <asememmanueladdo@gmial.com>
2023-02-21 09:12:49 -0800

    Lower case alpha bets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lower alphabets using
+ * Return: - Always return(0)
+ *
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++
+	}
+	_putchar('\n');
+}
