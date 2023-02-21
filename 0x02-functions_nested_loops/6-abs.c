a2ed77d938280dc2b320d51cca5b0553bd99f2b4
emmanueladdo <asememmanueladdo@gmial.com>
2023-02-21 11:33:14 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute
+ * Return: - abs of @n
+ * @n: int value
+ *
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	else
+		return (n);
+}
