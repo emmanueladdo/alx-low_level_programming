14f78973839ed594faac7b14ea38c709abfd8671
emmanueladdo <asememmanueladdo@gmial.com>
2023-02-22 11:45:16 -0800

    un-d

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,24 +7,39 @@
  */
 int main(void)
 {
-	unsigned long first = 1, second = 2, third;
-	int i;
+	int inc;
+	unsigned long n1 = 0, n2 = 1, n3;
+	unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
+	unsigned long h1, h2;
 
-	printf("%lu, %lu, ", first, second);/*prints first two*/
-		for (i = 2; i < 98; i++)/*loop start from  2*/
+	for (inc = 0; inc < 92; inc++)
+	{
+		n3 = n1 + n2;
+		printf("%lu, ", n3);
+		n1 = n2;
+		n2 = n3;
+	}
+	n1_h1 = n1 / 10000000000;
+	n2_h1 = n2 / 10000000000;
+	n1_h2 = n1 % 10000000000;
+	n2_h2 = n2 % 10000000000;
+	for (inc = 93; inc < 99; inc++)
+	{
+		h1 = n1_h1 + n2_h1;
+		h2 = n1_h2 + n2_h2;
+		if ((n1_h2 + n2_h2) > 9999999999)
 		{
-			if (i == 96)
-			{
-			third = first + second;
-			printf("%lu\n", third);
-			}
-			else
-			{
-				third = first + second;
-				printf("%lu, ", third);
-			}
-			first = second;
-			second = third;
+			h1 += 1;
+			h2 %= 10000000000;
 		}
-		return (0);
+		printf("%lu%lu", h1, h2);
+		if (inc != 98)
+			printf(", ");
+		n1_h1 = n2_h1;
+		n1_h2 = n2_h2;
+		n2_h1 = h1;
+		n2_h2 = h2;
+	}
+	printf("\n");
+	return (0);
 }
