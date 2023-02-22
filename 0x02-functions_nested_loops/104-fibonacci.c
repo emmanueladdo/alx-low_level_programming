88aab43b7b632ea6327ad51ef5557d4fc95d05a3
emmanueladdo <asememmanueladdo@gmial.com>
2023-02-22 11:31:40 -0800

    un-d

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,18 +10,18 @@ int main(void)
 	unsigned long first = 1, second = 2, third;
 	int i;
 
-	printf("%ld, %ld, ", first, second);/*prints first two*/
+	printf("%lu, %lu, ", first, second);/*prints first two*/
 		for (i = 2; i < 98; i++)/*loop start from  2*/
 		{
 			if (i == 96)
 			{
 			third = first + second;
-			printf("%li\n", third);
+			printf("%lu\n", third);
 			}
 			else
 			{
 				third = first + second;
-				printf("%li, ", third);
+				printf("%lu, ", third);
 			}
 			first = second;
 			second = third;
