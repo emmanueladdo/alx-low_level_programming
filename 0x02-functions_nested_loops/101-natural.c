ba675677e907d7b60baa7ab15c25668708a350b6
emmanueladdo <asememmanueladdo@gmial.com>
2023-02-22 08:46:44 -0800

    Natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * main - Entry Point for this program
+ * Return: return 0
+ * Description - computes and prints the sum of multiples of 3 or 5
+ */
+int main(void)
+{
+	int a = 1;
+	int sum = 0;
+
+		while (a < 1024)
+		{
+			if (a % 3 == 0)
+				sum += a;
+			else if (a % 5 == 0)
+				sum += a;
+				a++;
+			}
+			printf("%i\n", sum);
+}
