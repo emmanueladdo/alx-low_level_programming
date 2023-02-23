84c717c51c80792b4364888b1eed9fbf90d42337
emmanueladdo <asememmanueladdo@gmial.com>
2023-02-23 09:09:20 -0800

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - function for multiplecation
+ * Return: - multiplication of a and b
+ * @a: first argument
+ * @b: second arguemnet
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
