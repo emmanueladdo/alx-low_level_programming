15957d6f479e26fe815f255cae19cd3699574574
emmanueladdo <asememmanueladdo@gmial.com>
2023-02-27 07:01:05 -0800

    String lenth

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of a and b
+ * @a: first int type pointer
+ * @b: second int type pointer
+ * Return: no return value
+ */
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	while(s[counter] != '\0')
+	{
+		counter++;
+	}
+		return (counter);
+}
