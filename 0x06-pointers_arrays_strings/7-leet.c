e0f6d19a68aa2c75c2d2098c721c964db05fc245
emmanueladdo <asememmanueladdo@gmail.com>
2023-03-02 06:03:31 -0800

    Encoding

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encode a string into 1337
+ * @str: array of string characters
+ * Return: str
+ */
+char *leet(char *str)
+{
+	int i;
+
+		for (i = 0; str[i] != '\0'; i++)
+	{
+		while (str[i] == 'a' || str[i] == 'A')
+			str[i] = '4';
+		while (str[i] == 'e' || str[i] == 'E')
+			str[i] = '3';
+		while (str[i] == 'o' || str[i] == 'O')
+			str[i] = '0';
+		while (str[i] == 't' || str[i] == 'T')
+			str[i] = '7';
+		while (str[i] == 'l' || str[i] == 'L')
+			str[i] = '1';
+	}
+
+	return (str);
+}
