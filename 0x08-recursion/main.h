eb5eb1a5687a8e0365e432cbbbe1c25e52e77cc7
emmanueladdo <asememmanueladdo@gmail.com>
2023-03-09 00:34:26 -0800

    Try to understand this

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,4 +12,7 @@ int _putchar(char c);
 int is_palindrome(char *s);
 int my_pal(char *s, int l);
 int get_length(char *s);
+int my_pal(char *s, int l);
+int wildcmp(char *s1, char *s2);
+int findsrc(char *s, char c, int i, int p);
 #endif /* MAIN_H*/
