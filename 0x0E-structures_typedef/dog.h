b915261233bc190c3ed682457cafc9238c631676
emmanueladdo <asememmanueladdo@gmail.com>
2023-03-20 09:01:45 -0700

    fist struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,16 @@
-struct dog{
+#ifndef dog_h
+#define dog_h
+/**
+ *struct dog - struct for defination of a dog
+ *@name: charatcer string for the dog-name
+ *@age: int for dog age
+ *@owner: character string for dog owner-name
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
