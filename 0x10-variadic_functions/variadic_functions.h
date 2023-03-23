dd83044a1e9db64e4da78db6a297bce88815d654
emmanueladdo <asememmanueladdo@gmail.com>
2023-03-23 12:34:49 -0700

    the print all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,21 +3,24 @@
 #include <stdarg.h>
 
 /**
- * struct check - A new struct type defining a printer.
- * @symbol: A symbol representing a data type.
- * @print: A function pointer to a function that prints
+ * struct checker - A new struct type defining a printer.
+ * @type: A symbol representing a data type.
+ * @f: A function pointer to a function that prints
  *         a data type corresponding to symbol.
  */
-typedef struct check
+typedef struct checker
 {
-	char *symbol;
-	void (*print)(va_list arg);
+	char *type;
+	void (*f)();
 
-} check_t;
+} checker;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+void _printchar(va_list list);
+void _printstr(va_list list);
+void _printfloat(va_list list);
+void _printint(va_list list);
 #endif
