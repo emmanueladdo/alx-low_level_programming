518388c9fdd4d5dad2c3ff49487dbeb71076d551
emmanueladdo <asememmanueladdo@gmail.com>
2023-03-30 09:46:10 -0700

    correct

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		printf("[%d] %s\n", h->len, h->str ? h->str : "(nil)");
 		h = h->next;
 		count++;
 	}
