837c8b3b26239372e3de6e163510a82c6f391367
emmanueladdo <asememmanueladdo@gmail.com>
2023-05-18 10:28:32 -0700

    insert node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,112 +1,41 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_idx - Inserts a new node at a given position in a d-list.
- * @h: Double pointer to the head.
- * @idx: Index of the position.
- * @n: Value to store in the new node.
+ * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
+ *                            list at a given position.
+ * @h: A pointer to the head of the dlistint_t list.
+ * @idx: The position to insert the new node.
+ * @n: The integer for the new node to contain.
  *
- * Return: Address of the new node, or NULL if failed.
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
  */
-dlistint_t *insert_dnodeint_at_idx(dlistint_t **h, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *tmp = *h, *new;
+
 	if (idx == 0)
-		return (insert_at_start(h, n));
+		return (add_dnodeint(h, n));
 
-	if (*h == NULL)
-		return (NULL);
-
-	dlistint_t *current = *h;
-	unsigned int count = 0;
-
-	while (current != NULL && count < idx - 1)
+	for (; idx != 1; idx--)
 	{
-		current = current->next;
-		count++;
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
 	}
 
-	if (current == NULL)
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
 
-	if (current->next == NULL)
-		return (insert_at_end(h, n));
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
 
-	return (insert_in_middle(current, n));
+	return (new);
 }
-
-/**
- * insert_at_start - Inserts a new node at the start of a d-list.
- * @h: Double pointer to the head.
- * @n: Value to store in the new node.
- *
- * Return: Address of the new node.
- */
-dlistint_t *insert_at_start(dlistint_t **h, int n)
-{
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = *h;
-
-	if (*h != NULL)
-		(*h)->prev = new_node;
-
-	*h = new_node;
-	return (new_node);
-}
-
-/**
- * insert_at_end - Inserts a new node at the end of a d-list.
- * @h: Double pointer to the head.
- * @n: Value to store in the new node.
- *
- * Return: Address of the new node.
- */
-dlistint_t *insert_at_end(dlistint_t **h, int n)
-{
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->next = NULL;
-
-	dlistint_t *current = *h;
-
-	while (current->next != NULL)
-		current = current->next;
-
-	new_node->prev = current;
-	current->next = new_node;
-	return (new_node);
-}
-
-/**
- * insert_in_middle - Inserts a new node in the middle of a d-list.
- * @current: Pointer to the node after which to insert the new node.
- * @n: Value to store in the new node.
- *
- * Return: Address of the new node.
- */
-dlistint_t *insert_in_middle(dlistint_t *current, int n)
-{
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->prev = current;
-	new_node->next = current->next;
-
-	current->next->prev = new_node;
-	current->next = new_node;
-
-	return (new_node);
-}
-
