6671ce73f2f4bc607ebc7f99fdd4f90eea558ca7
emmanueladdo <asememmanueladdo@gmail.com>
2023-05-23 04:54:59 -0700

    git

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/emmanueladdo/alx-low_level_programming/blob/master/0x18-dynamic_libraries/icrack.so
+wget -P /tmp https://github.com/emmanueladdo/alx-low_level_programming/raw/master/0x18-dynamic_libraries/icrack.so
 export LD_PRELOAD=/tmp/icrack.so
