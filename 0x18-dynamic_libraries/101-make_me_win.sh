c560bbe7dbbf229af4ac00e4670e43909b24f92e
emmanueladdo <asememmanueladdo@gmail.com>
2023-05-23 22:01:51 -0700

    I crake imple

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/emmanueladdo/alx-low_level_programming/raw/master/0x18-dynamic_libraries/icrack.so
+wget -P /tmp/ https://raw.githubusercontent.com/emmanueladdo//alx-low_level_programming/master/0x18-dynamic_libraries/icrack.so
 export LD_PRELOAD="/tmp/icrack.so"
