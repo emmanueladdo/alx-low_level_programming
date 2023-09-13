71aa2a01915824c8312b3bf3ccb4141bb90ea516
root <root@DESKTOP-KUT53MR>
2023-09-13 13:32:31 +0000

    jump serach

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,11 @@
 #ifndef search_algo
 #define search_algo
 #include <stdio.h>
+#include <math.h>
 
 void print_array(int *array, size_t inc, size_t size);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif
